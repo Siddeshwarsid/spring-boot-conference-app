62bdb731506084bc51e31f48be161331049c7739
Nilesh Gule <vn_nilesh@yahoo.com>
2021-05-30 22:17:27 +0800

    updated powershell script

--- powershell/install-prometheus.ps1
+++ powershell/install-prometheus.ps1
@@ -3,12 +3,13 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 
 helm repo update
 
-Write-Host "Installing prometheus using Helm chart" -ForegroundColor Yellow
-helm install prometheus `
+Write-Host "Installing / upgrading prometheus using Helm chart" -ForegroundColor Yellow
+helm upgrade --install prometheus `
 prometheus-community/kube-prometheus-stack  `
 --create-namespace `
 --wait `
 --namespace monitoring
+
 
 Write-Host "Display pods..." -ForegroundColor Yellow
 kubectl --namespace monitoring `
@@ -16,3 +17,11 @@ get pods `
 --selector "release=prometheus"
 
 # helm uninstall prometheus --namespace monitoring
+
+# kubectl port-forward prometheus-prometheus-kube-prometheus-prometheus-0 `
+# 9090 `
+# --namespace monitoring
+#
+# kubectl port-forward prometheus-grafana-7b764c9d78-xjtr5 `
+# 3000 `
+# --namespace monitoring
