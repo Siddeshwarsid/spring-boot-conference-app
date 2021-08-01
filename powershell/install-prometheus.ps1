27d18c38de9feef84a0714caebbca47add78d8c3
Nilesh Gule <vn_nilesh@yahoo.com>
2021-08-01 18:55:26 +0800

    updated powershell script for prometheus installation, db service updates

--- powershell/install-prometheus.ps1
+++ powershell/install-prometheus.ps1
@@ -34,48 +34,48 @@ get pods `
 # prometheus-kube-prometheus-prometheus.monitoring.svc.cluster.local:9090
 
 # Bitnami charts
-helm repo add azure-marketplace https://marketplace.azurecr.io/helm/v1/repo
-
-helm repo update
-
-helm upgrade --install prometheus `
-azure-marketplace/kube-prometheus  `
---create-namespace `
---wait `
---namespace monitoring
-
-helm repo add bitnami https://charts.bitnami.com/bitnami
-
-helm repo update
-
- helm upgrade --install grafana bitnami/grafana `
- --set admin.user=admin `
- --set metrics.enabled=true `
- --set metrics.serviceMonitor.enabled=true `
- --create-namespace `
- --wait `
- --namespace monitoring
-
- Prometheus url
- prometheus-kube-prometheus-prometheus.monitoring.svc.cluster.local
-
- prometheus port 9090
-
-
- kubectl port-forward --namespace monitoring svc/prometheus-kube-prometheus-prometheus 9090:9090
-
-alter manager port 9093
- prometheus-kube-prometheus-alertmanager.monitoring.svc.cluster.local
-
- kubectl port-forward --namespace monitoring svc/prometheus-kube-prometheus-alertmanager 9093:9093
-
- grafana port 3000
-  kubectl --namespace monitoring port-forward svc/prometheus-grafana 80:80
-
-   echo "User: admin"
-      echo "Password: $(kubectl get secret grafana-admin --namespace monitoring -o jsonpath="{.data.GF_SECURITY_ADMIN_PASSWORD}" | base64 --decode)"
-
-      kubectl get secret prometheus-grafana --namespace monitoring -o jsonpath="{.data.admin-password}"
+# helm repo add azure-marketplace https://marketplace.azurecr.io/helm/v1/repo
+#
+# helm repo update
+#
+# helm upgrade --install prometheus `
+# azure-marketplace/kube-prometheus  `
+# --create-namespace `
+# --wait `
+# --namespace monitoring
+#
+# helm repo add bitnami https://charts.bitnami.com/bitnami
+#
+# helm repo update
+#
+#  helm upgrade --install grafana bitnami/grafana `
+#  --set admin.user=admin `
+#  --set metrics.enabled=true `
+#  --set metrics.serviceMonitor.enabled=true `
+#  --create-namespace `
+#  --wait `
+#  --namespace monitoring
+#
+#  Prometheus url
+#  prometheus-kube-prometheus-prometheus.monitoring.svc.cluster.local
+#
+#  prometheus port 9090
+#
+#
+#  kubectl port-forward --namespace monitoring svc/prometheus-kube-prometheus-prometheus 9090:9090
+#
+# alter manager port 9093
+#  prometheus-kube-prometheus-alertmanager.monitoring.svc.cluster.local
+#
+#  kubectl port-forward --namespace monitoring svc/prometheus-kube-prometheus-alertmanager 9093:9093
+#
+#  grafana port 3000
+#   kubectl --namespace monitoring port-forward svc/prometheus-grafana 80:80
+#
+#    echo "User: admin"
+#       echo "Password: $(kubectl get secret grafana-admin --namespace monitoring -o jsonpath="{.data.GF_SECURITY_ADMIN_PASSWORD}" | base64 --decode)"
+#
+#       kubectl get secret prometheus-grafana --namespace monitoring -o jsonpath="{.data.admin-password}"
 
 # Working version of Springboot application dashboard
 #https://grafana.com/grafana/dashboards/11955
